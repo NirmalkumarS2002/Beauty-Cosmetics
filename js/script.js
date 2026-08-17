68281b73cab0e070f04dc27712100ab08b7d4376
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-08-17 14:38:06 +0530

    nav bar created

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const menubtn = document.querySelector(".menu-btn")
+const closebtn = document.querySelector(".close-btn")
+const headsidebar = document.querySelector(".head-sidebar")
+const body = document.querySelector("body")
+const header = document.querySelector(".header")
+
+menubtn.addEventListener("click", () => {
+    headsidebar.classList.add("active")
+
+})
+
+closebtn.addEventListener("click", () => {
+    headsidebar.classList.remove("active")
+})
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("active")
+    }
+
+    else {
+        header.classList.remove("active")
+    }
+})
